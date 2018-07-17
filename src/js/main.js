d88817d0f05f2806ed8432d774c5f565f6e1ee74
Zaharakatd22 <zaharakatd22@gmail.com>
2018-07-17 17:17:46 +0400

    Готов слайдер, нужно только наполнить

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-	$('.main__promoblock-btn').on("click", function() { $('.overlay').show() });
-	$('.promoblock__text-btn').on("click", function() { $('.overlay').show() });
-	$('.header__ordercall').on("click", function() { $('.overlay2').show() });
-	$('.footer__contact-ordercall').on("click", function() { $('.overlay2').show() });	
-	$('.popup__close').on("click", function() { $('.overlay').hide() });
-	$('.popup__close').on("click", function() { $('.overlay2').hide() });
+	$('.feedback__slider').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+	});
+    $('.main__promoblock-btn').on("click", function() { $('.overlay').show() });
+    $('.promoblock__text-btn').on("click", function() { $('.overlay').show() });
+    $('.header__ordercall').on("click", function() { $('.overlay2').show() });
+    $('.footer__contact-ordercall').on("click", function() { $('.overlay2').show() });
+    $('.popup__close').on("click", function() { $('.overlay').hide() });
+    $('.popup__close').on("click", function() { $('.overlay2').hide() });
+    $('.overlay').on("click", function() { $('.overlay').hide() });
+    $('.overlay').on("click", function() { $('.overlay2').hide() });
+    $('.overlay2').on("click", function() { $('.overlay').hide() });
+    $('.overlay2').on("click", function() { $('.overlay2').hide() });
 });
